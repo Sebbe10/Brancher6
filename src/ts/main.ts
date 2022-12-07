a7c5e27b95eafbdbca645eea8d7b93e1d0b44464
Sebastian Bjelkstam <sebastian.bjelkstam@gmail.com>
2022-12-07 10:14:03 +0100

    Brancher6

---
+++ src/ts/main.ts
@@ -0,0 +1,39 @@
+class Person {
+  interest: string;
+  constructor(interest: string) {
+    this.interest = interest;
+  }
+}
+
+let myul = new Person("Hej");
+
+let myul2 = new Person("Tja");
+
+let names = 0;
+
+let ulTag = document.getElementById("myUL");
+
+let myuls = ["Hej", "Tja"];
+
+for (let i = 0; i < myuls.length; i++) {
+  console.log(myuls);
+  let li = document.createElement("li");
+
+  var span = document.createElement("span");
+
+  var txt = document.createTextNode("\u00D7");
+
+  let ull = document.createElement("button");
+  li.innerHTML = myuls[i];
+
+  ull.appendChild(span);
+  li.appendChild(span);
+  ulTag?.appendChild(li);
+
+  span.className = "close";
+  span.appendChild(txt);
+
+  span.addEventListener("click", () => {
+    li.remove();
+  });
+}
